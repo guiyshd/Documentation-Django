6f6c2eb154ac99bf9907c07f460eaff48f935733
Guilherme Yoshida <yoshida.guilherme80@gmail.com>
2020-07-13 22:02:28 -0300

    login e register

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -3,3 +3,11 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+
+def login(request):
+    return HttpResponse("Login page.")
+
+
+def register(request):
+    return HttpResponse("Register.")
