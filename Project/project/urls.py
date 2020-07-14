5a5018822a9a13d0a9dcc9af43c14dc844c7de3d
Guilherme Yoshida <yoshida.guilherme80@gmail.com>
2020-07-13 21:26:35 -0300

    Urls & Templates

--- Project/project/urls.py
+++ Project/project/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('index', TemplateView.as_view(template_name='index.html')),
+    path('about', TemplateView.as_view(template_name='about.html')),
+    path('contact', TemplateView.as_view(template_name='contact.html')),
+    path('', TemplateView.as_view(template_name='home.html'))
 ]
